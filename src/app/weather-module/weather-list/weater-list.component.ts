03e096d7221892fd0e2a5260dd83211f387dc4b6
Bohdan Artemenko <iomtt94@gmail.com>
2017-10-04 18:24:33 +0300

    fuuuuuuuuuuuuuuuuuuuuuuuuuuuucccccccccccccccccccccccckkkkkkkkkk

---
+++ src/app/weather-module/weather-list/weater-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-weater-list',
+  templateUrl: './weater-list.component.html',
+  styleUrls: ['./weater-list.component.css']
+})
+export class WeaterListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
